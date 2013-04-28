cbd8ebd5fa91dfdd7c7f430994bb30e3e190410b
Melody Shar <missxmelody@gmail.com>
2013-04-27 21:52:18 -0700

    more cleanup

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -39,121 +39,122 @@
 </head>
 
 <body>
-	<section id="home" class="container-fluid pages">
-    <div class="dots">
-     <div class="container">
-        <img id="logo" src="img/logo.png"> 
-     	<div class="row pos-relative hgt700"><!-- We need fix height for this <div> because we have image in absolute position. -->
-        	<div class="span8">
-			<h1 class="txt-white mrg50T pdg50T mrg40B mob-txt-center">
-                	Hi <span class="txt-yellow">I'm Greg</span>,<br />A Freelance Graphic/Web Designer<br />From Mumbai.
-                </h1>
-			./<p class="txt-white txt-font2 txt24 mrg20T mob-txt-center">I don't think outside of the box;<br />I think of what I can do with the box.</p>
-			<p class="mrg50T mob-txt-center"><a href="#work" class="btn btn-simple bg-yellow txt18 txt-font2 txt-blue">Have a look at my work <i class="icon-circle-arrow-down txt-blue"></i></a></p>
-            </div><!-- span8 END -->
-      </div>
-    </div><!--container-->
-  </div>
-</section><!-- home END -->
-    
-<section id="about" class="container-fluid pages">
-  <div class="container">
-    <div class="row">
-      <div class="span4">
-      </div>
-    </div>
-    <div class="row">
-    <article class="span4 center">
-      <h1 class="pg-title center"><span>About</span></h1>
-      <img id="profile-img" src="img/greg.jpg" class="img1 img-circle" alt="" />
-      <p class="mrg20B"><strong>Shane Grace</strong> is a full time <strong>Web/Graphic designer & Illustrator</strong>. I live in <strong>Mumbai, India</strong>. Phasellus vulputate iaculis congue. Donec ultrices, leo in fermentum accumsan, dolor eros condimentum arcu pretium rutrum diam  habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas facilisis blandit ligula dictum et vitae rutrum lorem velit nec ligula. Please take a moment to review selections of my work and feel free to contact me for further information on my background, skills and experience.</p>
-      <p><a href="javascript:void(0);" class="btn btn-styled txt16 txt-cap"><i class="icon-download-alt"></i> <strong>Download Resume</strong></a></p>                    
-    </article><!-- Mid Col -->                
-  	<article class="span4 offset1 mob-txt-center">
-    	<h3 class="tags">My Skills</h3>
-      <p class="mrg20B">
-      	<strong class="txt16 txt-darkgrey">UX Design</strong><br />
-      	Research Plans / Prototypes / User Testing / Interaction Design / Visual Design
-      </p>
-      <p class="mrg20B">
-      	<strong class="txt16 txt-darkgrey">Development</strong><br />
-      	XHTML / HTML5 / CSS3 / WordPress Implimentation
-      </p>
-      <p class="mrg20B">
-      	<strong class="txt16 txt-darkgrey">Digital and Printing</strong><br />
-      	Logo Design / Illustration / Business Stationery / Interactive ads / Emailer / Microsites
-      </p>
-      <p class="mrg20B">
-      	<strong class="txt16 txt-darkgrey">Team/Project Management</strong><br />
-      	Lead Planning / Ability to Multi-Task / Prioritize projects / Implementation project / project Budget and Resource Allocation
-      </p>
-        <!-- My Skills END -->
-    	<h3 class="tags">Tools I Use</h3>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Photoshop</strong>
-        <span class="txt12"> - To design website and mobile site/app</span>
-      </p>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Dreamviewer</strong>
-        <span class="txt12"> - Edit HTML/JS</span>
-      </p>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Adobe Illustrator</strong>
-        <span class="txt12"> - For vector design</span>
-      </p>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> FileZilla </strong>
-        <span class="txt12"> - For uploading files on server</span>
-      </p>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> TreePad Asia</strong>
-        <span class="txt12"> - To manage notes</span>
-      </p>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> iPhone</strong>
-        <span class="txt12"> - For testing sites/apps on mobile</span>
-      </p>
-      <p>
-      	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Producteev.com</strong>
-        <span class="txt12"> - To manage projects</span>
-      </p>
-        <!-- Tools I Use END -->
-    </article><!-- Left Col -->
 
-  	<article class="span3 mob-txt-center">
-    	<h3 class="tags">Experience</h3>
-      <p class="mrg20B">
-      	<strong class="txt-darkgrey txt16">Freelance</strong><br />
-          <strong class="txt16">2009- Current</strong><br />
-          <span>Currently working from my office in Mumbai, with my colleague.</span>
-      </p>
-      <p class="mrg20B">
-      	<strong class="txt-darkgrey txt16">ABC Tech Pvt. Ltd.</strong><br />
-          <strong class="txt16">2007- 2009 | Design Team Manager</strong><br />
-          <span>Job description porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur adipisci velit.</span>
-      </p>
-      <p class="mrg20B">
-      	<strong class="txt-darkgrey txt16">Attixx Core Pvt. Ltd.</strong><br />
-          <strong class="txt16">2004- 2007 | Web Designer</strong><br />
-          <span>Job description quia dolor sit porro quisquam est qui consectetur adipisci velit dolorem ipsum amet.</span>
-      </p>
-        
-        
-    	<h3 class="tags">Awards</h3>
+  <section id="home" class="container-fluid pages">
+    <div class="dots">
+      <div class="container">
+        <img id="logo" src="img/logo.png"> 
+     	  <div class="row pos-relative hgt700"><!-- We need fix height for this <div> because we have image in absolute position. -->
+        	<div class="span8">
+  		      <h1 class="txt-white mrg50T pdg50T mrg40B mob-txt-center">
+            Hi <span class="txt-yellow">I'm Greg</span>,<br />A Freelance Graphic/Web Designer<br />From Mumbai.
+            </h1>
+            <p class="txt-white txt-font2 txt24 mrg20T mob-txt-center">I don't think outside of the box;<br />I think of what I can do with the box.</p>
+            <p class="mrg50T mob-txt-center"><a href="#work" class="btn btn-simple bg-yellow txt18 txt-font2 txt-blue">Have a look at my work <i class="icon-circle-arrow-down txt-blue"></i></a></p>
+          </div><!-- span8 END -->
+        </div>
+      </div><!--container-->
+    </div>
+  </section><!-- home END -->
+      
+  <section id="about" class="container-fluid pages">
+    <div class="container">
+      <div class="row">
+        <div class="span4">
+        </div>
+      </div>
+      <div class="row">
+      <article class="span4 center">
+        <h1 class="pg-title center"><span>About</span></h1>
+        <img id="profile-img" src="img/greg.jpg" class="img1 img-circle" alt="" />
+        <p class="mrg20B"><strong>Shane Grace</strong> is a full time <strong>Web/Graphic designer & Illustrator</strong>. I live in <strong>Mumbai, India</strong>. Phasellus vulputate iaculis congue. Donec ultrices, leo in fermentum accumsan, dolor eros condimentum arcu pretium rutrum diam  habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas facilisis blandit ligula dictum et vitae rutrum lorem velit nec ligula. Please take a moment to review selections of my work and feel free to contact me for further information on my background, skills and experience.</p>
+        <p><a href="javascript:void(0);" class="btn btn-styled txt16 txt-cap"><i class="icon-download-alt"></i> <strong>Download Resume</strong></a></p>                    
+      </article><!-- Mid Col -->                
+    	<article class="span4 offset1 mob-txt-center">
+      	<h3 class="tags">My Skills</h3>
         <p class="mrg20B">
-            <span class="txt-orange">Site of the Month (BSCD Site)</span><br />
-            <a href="javascript:void(0);">abcexpyr.com</a> - Jun 2012<br />
-            <span class="txt12">Peor dolor sit porro quisquam est qui consectetur adipisci velit dolorem ipsum amet.</span>
+        	<strong class="txt16 txt-darkgrey">UX Design</strong><br />
+        	Research Plans / Prototypes / User Testing / Interaction Design / Visual Design
+        </p>
+        <p class="mrg20B">
+        	<strong class="txt16 txt-darkgrey">Development</strong><br />
+        	XHTML / HTML5 / CSS3 / WordPress Implimentation
+        </p>
+        <p class="mrg20B">
+        	<strong class="txt16 txt-darkgrey">Digital and Printing</strong><br />
+        	Logo Design / Illustration / Business Stationery / Interactive ads / Emailer / Microsites
+        </p>
+        <p class="mrg20B">
+        	<strong class="txt16 txt-darkgrey">Team/Project Management</strong><br />
+        	Lead Planning / Ability to Multi-Task / Prioritize projects / Implementation project / project Budget and Resource Allocation
+        </p>
+          <!-- My Skills END -->
+      	<h3 class="tags">Tools I Use</h3>
+        <p>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Photoshop</strong>
+          <span class="txt12"> - To design website and mobile site/app</span>
         </p>
         <p>
-            <span class="txt-orange">Best UI Design (RHWA Site)</span><br />
-            <a href="javascript:void(0);">opqrswrd.com</a> - Oct 2012<br />
-            <span class="txt12">Dolor sit porro quisquam est qui consectetur adipisci velit dolorem ipsum amet.</span>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Dreamviewer</strong>
+          <span class="txt12"> - Edit HTML/JS</span>
         </p>
-      </article><!-- Right Col -->
-    </div><!-- row END -->
-  </div><!-- container END -->
-</section><!-- About END -->
+        <p>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Adobe Illustrator</strong>
+          <span class="txt12"> - For vector design</span>
+        </p>
+        <p>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> FileZilla </strong>
+          <span class="txt12"> - For uploading files on server</span>
+        </p>
+        <p>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> TreePad Asia</strong>
+          <span class="txt12"> - To manage notes</span>
+        </p>
+        <p>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> iPhone</strong>
+          <span class="txt12"> - For testing sites/apps on mobile</span>
+        </p>
+        <p>
+        	<strong class="txt-darkgrey"><i class="icon-angle-right txt-orange"></i> Producteev.com</strong>
+          <span class="txt12"> - To manage projects</span>
+        </p>
+          <!-- Tools I Use END -->
+      </article><!-- Left Col -->
+
+    	<article class="span3 mob-txt-center">
+      	<h3 class="tags">Experience</h3>
+        <p class="mrg20B">
+        	<strong class="txt-darkgrey txt16">Freelance</strong><br />
+            <strong class="txt16">2009- Current</strong><br />
+            <span>Currently working from my office in Mumbai, with my colleague.</span>
+        </p>
+        <p class="mrg20B">
+        	<strong class="txt-darkgrey txt16">ABC Tech Pvt. Ltd.</strong><br />
+            <strong class="txt16">2007- 2009 | Design Team Manager</strong><br />
+            <span>Job description porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur adipisci velit.</span>
+        </p>
+        <p class="mrg20B">
+        	<strong class="txt-darkgrey txt16">Attixx Core Pvt. Ltd.</strong><br />
+            <strong class="txt16">2004- 2007 | Web Designer</strong><br />
+            <span>Job description quia dolor sit porro quisquam est qui consectetur adipisci velit dolorem ipsum amet.</span>
+        </p>
+          
+          
+      	<h3 class="tags">Awards</h3>
+          <p class="mrg20B">
+              <span class="txt-orange">Site of the Month (BSCD Site)</span><br />
+              <a href="javascript:void(0);">abcexpyr.com</a> - Jun 2012<br />
+              <span class="txt12">Peor dolor sit porro quisquam est qui consectetur adipisci velit dolorem ipsum amet.</span>
+          </p>
+          <p>
+              <span class="txt-orange">Best UI Design (RHWA Site)</span><br />
+              <a href="javascript:void(0);">opqrswrd.com</a> - Oct 2012<br />
+              <span class="txt12">Dolor sit porro quisquam est qui consectetur adipisci velit dolorem ipsum amet.</span>
+          </p>
+        </article><!-- Right Col -->
+      </div><!-- row END -->
+    </div><!-- container END -->
+  </section><!-- About END -->
 	
   <section id="work" class="container-fluid pages">
   	<div class="container">
@@ -274,7 +275,7 @@
   <section id="services" class="container-fluid pages">
   	<div class="container">
       <h1 class="pg-title txt-yellow"><span>Services</span></h1> 
-      <div class="row serv-info">         	
+      <div class="row serv-info">
         <article class="span3 serv-col txt-center txt-white">
           <div class="circle"><i class="icon-magic"></i></div>
           <p class="txt-font2 txt24 txt-cap">Designing</p>
